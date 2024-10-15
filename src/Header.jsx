8d3e1bfa09d42bc567c3ef069673d6d088d04d14
Alfinanasdeen <alfinanasdeen@gmail.com>
2024-10-15 11:59:00 +0530

    commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,34 +1,32 @@
-import { Link } from "react-router-dom";
-import { useContext, useEffect } from "react";
-import { UserContext } from "./UserContext";
+import {Link} from "react-router-dom";
+import {useContext, useEffect} from "react";
+import {UserContext} from "./UserContext";
 
 export default function Header() {
-  const { setUserInfo, userInfo } = useContext(UserContext);
+  const {setUserInfo,userInfo} = useContext(UserContext);
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_BASE_URL}/profile`, {
-      credentials: "include", // Include credentials
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json(); // Parse JSON if response is ok
-      })
-      .then((userInfo) => {
-        setUserInfo(userInfo); // Update user info in context
-      })
-      .catch((error) => {
-        console.error("Error fetching user info:", error);
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo);
       });
+    });
   }, []);
+
+  function logout() {
+    fetch(`${import.meta.env.VITE_API_BASE_URL}/logout`, {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
 
   const username = userInfo?.username;
 
   return (
     <header>
-      <Link to="/" className="logo">
-        MyBlog
-      </Link>
+      <Link to="/" className="logo">MyBlog</Link>
       <nav>
         {username && (
           <>
